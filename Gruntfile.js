916ed0df6d28272ef313cc09657bfded42ecb6a4
Jon Schlinkert <dev@sellside.com>
2013-07-06 07:17:11 -0400

    remove tasks and junk from Gruntfile that is no longer needed since we moved examples to another repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,68 +51,21 @@ module.exports = function(grunt) {
     clean: {
       helpers: ['<%= coffee.helpers.dest %>/**/*.js'],
       tests:  ['examples/result/**/*.{html,md}']
-    },
-
-    // Copy helpers to assemble in node_modules and
-    // helper-lib-examples for testing
-    copy: {
-      main: {
-        files: [
-          { expand: true, cwd: './lib', src: ['**'], dest: 'node_modules/assemble/node_modules/helper-lib/'}
-        ]
-      }
-    },
-
-    subgrunt: {
-      copy: 'Gruntfile.js',
-      examples: '../helper-lib-examples/Gruntfile.js'
     }
+
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-mocha-test');
 
   // By default, build templates using helpers and run all tests.
-  grunt.registerTask('default', [
-    'clean',
-    'coffee',
-    'copy'
-  ]);
-  grunt.registerTask('helpers', [
-    'clean',
-    'coffee',
-    'copy',
-    'subgrunt'
-  ]);
+  grunt.registerTask('default', ['clean', 'coffee']);
 
   // Build templates using helpers and run all tests.
-  grunt.registerTask('test', [
-    'coffee',
-    'mochaTest'
-  ]);
+  grunt.registerTask('test',    ['default', 'mochaTest']);
 
-
-  // Run Gruntfiles in given directories.
-  grunt.registerMultiTask('subgrunt', 'Run a sub-gruntfile.', function() {
-    var path = require('path');
-    grunt.util.async.forEachSeries(this.filesSrc, function(gruntfile, next) {
-      grunt.util.spawn({
-        grunt: true,
-        args: ['--gruntfile', path.resolve(gruntfile)]
-      }, function(error, result) {
-        if (error) {
-          grunt.log.error(result.stdout).writeln();
-          next(new Error('Error running sub-gruntfile "' + gruntfile + '".'));
-        } else {
-          grunt.verbose.ok(result.stdout);
-          next();
-        }
-      });
-    }, this.async());
-  });
 };
 
