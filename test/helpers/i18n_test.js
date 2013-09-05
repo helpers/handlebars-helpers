23a273d0455c41a982b7a38d8cedcf80d0b0801f
Jon Schlinkert <dev@sellside.com>
2013-09-05 14:23:52 -0400

    fixes i18n tests

--- test/helpers/i18n_test.js
+++ test/helpers/i18n_test.js
@@ -6,26 +6,30 @@
   Handlebars = require('handlebars');
 
   require('../../lib/helpers/helpers-i18n').register(Handlebars, {});
-  
-  context = {
-    language: 'en',
-    en: {key: 'value'},
-    fr: {key: 'valeur'}
-  }
 
-  describe('i18n', function() {
-    return describe('{{urlresolve base href}}', function() {
-      it('should take a key and return for the default language', function() {
+  context = {
+    language: "en",
+    en: {
+      key: "value"
+    },
+    fr: {
+      key: "valeur"
+    }
+  };
+
+  describe("i18n", function() {
+    return describe("{{#i18n}}{{/i18n}}", function() {
+      it("should take a key and return for the default language", function() {
         var source, template;
-        source = '{{#i18n "key"}}';
+        source = "{{#i18n 'key'}}{{/i18n}}";
         template = Handlebars.compile(source);
-        return template().should.equal("value");
+        return template(context).should.equal("value");
       });
-      return it('should take a key and return for the override language', function() {
+      return it("should take a key and return for the override language", function() {
         var source, template;
-        source = '{{#i18n "key" language="fr"}}';
+        source = "{{#i18n 'key' language='fr'}}{{/i18n}}";
         template = Handlebars.compile(source);
-        return template().should.equal("valeur");
+        return template(context).should.equal("valeur");
       });
     });
   });
