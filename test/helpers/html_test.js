d4b4e39c9f0db00514cd098cc1f0633206e46c65
ChiperSoft <chiper@chipersoft.com>
2014-01-04 08:25:14 -0800

    Fixed failing html nap test due to bad relative path for publicDir

--- test/helpers/html_test.js
+++ test/helpers/html_test.js
@@ -22,7 +22,7 @@ describe('html', function() {
 
     before(function() {
       nap({
-        publicDir: '/test/actual/',
+        publicDir: './test/actual/',
         mode: 'production',
         assets: {
           js: {
