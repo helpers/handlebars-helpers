6d9199ba5512940e566438cadb87cd9ec252e5d5
Brian Woodward <brian.woodward@gmail.com>
2014-03-27 14:32:57 -0400

    replacing date helpers and tests

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -7,55 +7,61 @@
  */
 
 
-  /**
-   * {{formatData}}
-   * Port of formatDate-js library (http://bit.ly/18eo2xw)
-   * @param  {[type]} date   [description]
-   * @param  {[type]} format [description]
-   * @return {[type]}        [description]
-   */
-  Library.addHelper('formatDate', function (date, format) {
+Library.addHelper('formatDate', function (date, format) {
+  if (!Utils.isUndefined(date)) {
+    date = Utils.result(date);
+    format = Utils.result(format);
     date = new Date(date);
     return Dates.format(date, format);
-  });
+  } else {
+    return Utils.err('{{formatDate}} takes two arguments (string|number|date, string).');
+  }
+});
 
-  /**
-   * {{now}}
-   * @param  {[type]} format [description]
-   * @return {[type]}        [description]
-   */
-  Library.addHelper('now', function (format) {
-    var date = new Date();
-    if (Utils.isUndefined(format)) {
-      return date;
-    } else {
-      return Dates.format(date, format);
-    }
-  });
+Library.addHelper('now', function (format) {
+  if (!Utils.isUndefined(format)) {
+    format = Utils.result(format);
+  }
+  var date = new Date();
+  if (Utils.isUndefined(format)) {
+    return date;
+  } else {
+    return Dates.format(date, format);
+  }
+});
 
-  /**
-   * {{timeago}}
-   * Modified version of http://bit.ly/18WwJYf
-   * @param  {[type]} date [description]
-   * @return {[type]}      [description]
-   */
-  Library.addHelper('timeago', function (date) {
+Library.addHelper('timeago', function (date) {
+  if (!Utils.isUndefined(date)) {
+    date = Utils.result(date);
     date = new Date(date);
     var seconds = Math.floor((new Date() - date) / 1000);
     var interval = Math.floor(seconds / 31536000);
-    if (interval > 1) {return "" + interval + " years ago"; }
+    if (interval > 1) {
+      return "" + interval + " years ago";
+    }
     interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {return "" + interval + " months ago"; }
+    if (interval > 1) {
+      return "" + interval + " months ago";
+    }
     interval = Math.floor(seconds / 86400);
-    if (interval > 1) {return "" + interval + " days ago"; }
+    if (interval > 1) {
+      return "" + interval + " days ago";
+    }
     interval = Math.floor(seconds / 3600);
-    if (interval > 1) {return "" + interval + " hours ago"; }
+    if (interval > 1) {
+      return "" + interval + " hours ago";
+    }
     interval = Math.floor(seconds / 60);
-    if (interval > 1) {return "" + interval + " minutes ago"; }
+    if (interval > 1) {
+      return "" + interval + " minutes ago";
+    }
     if (Math.floor(seconds) === 0) {
       return 'Just now';
     } else {
       return Math.floor(seconds) + ' seconds ago';
     }
-  });
+  } else {
+    return Utils.err('{{timeago}} takes one argument (string|number|date).');
+  }
+});
 
