856b7cfe6d6f23e78bf59b7fdec90f8a69a87e3e
Brian Woodward <brian.woodward@gmail.com>
2014-03-27 14:35:16 -0400

    replacing html helpers

--- dist/commonjs/main.js
+++ dist/commonjs/main.js
@@ -543,86 +543,30 @@ Library.addHelper('timeago', function (date) {
  */
 
 
+Library.addHelper('ul', function (context, options) {
+  return ("<ul " + (HTML.parseAttributes(options.hash)) + ">") + context.map(function (item) {
+    return "<li>" + (options.fn(Utils.result(item))) + "</li>";
+  }).join('\n') + "</ul>";
+});
 
-    /**
-     * {{css}}
-     * Add an array of <link></link> tags. Automatically resolves
-     * relative paths to `options.assets` in the Assemble task.
-     * @param  {[type]} context [description]
-     * @return {[type]}         [description]
-     */
-    Library.addHelper('css', function (context) {
-      if (!Array.isArray(context)) {
-        context = [context];
-      }
-      return new Utils.safeString(context.map(function (item) {
-        var ext = Utils.getExt(item);
-        var css = '<link rel="stylesheet" href="' + options.assets + '/css/' + item + '">';
-        var less = '<link rel="stylesheet/less" href="' + options.assets + '/less/' + item + '">';
-        switch (ext) {
-          case "less":
-            return less;
-          case "css":
-            return css;
-          default:
-            return css;
-        }
-      }).join("\n"));
-    });
+Library.addHelper('ol', function (context, options) {
+  return ("<ol " + (HTML.parseAttributes(options.hash)) + ">") + context.map(function (item) {
+    return "<li>" + (options.fn(Utils.result(item))) + "</li>";
+  }).join('\n') + "</ol>";
+});
 
-    /**
-     * {{js "src/to/*.js"}}
-     * @param  {[type]} context [description]
-     * @return {[type]}         [description]
-     */
-    Library.addHelper('js', function (context) {
-      if (!Array.isArray(context)) {
-        context = [context];
-      }
-      return new Utils.safeString(context.map(function (item) {
-        var ext = Utils.getExt(item);
-        var js = '<script src="' + options.assets + '/js/' + item + '"></script>';
-        var coffee = '<script type="text/coffeescript" src="' + options.assets + '/js/' + item + '"></script>';
-        switch (ext) {
-          case "js":
-            return js;
-          case "coffee":
-            return coffee;
-          default:
-            return js;
-        }
-      }).join("\n"));
-    });
-
-    /**
-     * {{#ul}}
-     *   <li></li>
-     * {{/ul}}
-     * Block helper for creating unordered lists.
-     * @param  {[type]} context [description]
-     * @param  {[type]} opts    [description]
-     * @return {[type]}         [description]
-     */
-    Library.addHelper('ul', function (context, opts) {
-      return ("<ul " + (HTML.parseAttributes(opts.hash)) + ">") + context.map(function (item) {
-        return "<li>" + (opts.fn(item)) + "</li>";
-      }).join("\n") + "</ul>";
-    });
-
-    /**
-     * {{#ol}}
-     *   <li></li>
-     * {{ol}}
-     * Block helper for creating ordered lists.
-     * @param  {[type]} context [description]
-     * @param  {[type]} opts    [description]
-     * @return {[type]}         [description]
-     */
-    Library.addHelper('ol', function (context, opts) {
-      return ("<ol " + (HTML.parseAttributes(opts.hash)) + ">") + context.map(function (item) {
-        return "<li>" + (opts.fn(item)) + "</li>";
-      }).join("\n") + "</ol>";
-    });
+Library.addHelper('br', function (count, options) {
+  var br = '<br>';
+  if (!Utils.isUndefined(count)) {
+    var i = 0;
+    count = Utils.result(count);
+    while (i < (parseFloat(count)) - 1) {
+      br += '<br>';
+      i++;
+    }
+  }
+  return Utils.safeString(br);
+});
 
 
 	// Source File: ./src/helpers/inflections.js
