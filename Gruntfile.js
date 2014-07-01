c8071c8de173837204cb0617aafecd47aadf0e7e
makotot <makoto.ttn@gmail.com>
2014-07-01 23:05:57 +0900

    remove readme task

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,12 +86,6 @@ module.exports = function(grunt) {
       }
     },
 
-    readme: {
-      options: {
-        metadata: ['docs/*.json']
-      }
-    },
-
     compress: {
       zip: {
         options: {
