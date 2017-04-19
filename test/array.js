437a1bd0fdd6fa741633d62c99433d8836772099
Parker <youngyongpark@gmail.com>
2017-04-19 19:48:04 +0900

    add `.lengthGte` array helper.

--- test/array.js
+++ test/array.js
@@ -242,6 +242,18 @@ describe('array', function() {
     });
   });
 
+  describe('lengthGte', function() {
+    it('should render the first block if length is greater than or equal to the given number', function() {
+      var fn = hbs.compile('{{#lengthGte array 8}}AAA{{else}}BBB{{/lengthGte}}');
+      fn(context).should.equal('AAA');
+    });
+
+    it('should render the inverse block if length is not greater than or equal to the given number', function() {
+      var fn = hbs.compile('{{#lengthGte array 9}}AAA{{else}}BBB{{/lengthGte}}');
+      fn(context).should.equal('BBB');
+    });
+  });
+
   describe('some', function() {
     it('should render the first block if the callback returns true', function() {
       var ctx = {array: ['a', 'b', 'c']};
