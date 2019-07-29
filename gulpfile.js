14c5f10cd85c9f07b31921cc62526c42b5a2a84f
Paul Welsh <spacedawwwg@gmail.com>
2019-07-29 17:06:58 +0100

    audit fix and update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,20 @@ var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
 var unused = require('gulp-unused');
 
-gulp.task('coverage', ['eslint'], function() {
+gulp.task('eslint', function() {
+  return gulp.src(['*.js', 'lib/**/*.js', 'test/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('coverage', gulp.series('eslint'), function() {
   return gulp.src(['index.js', 'lib/**/*.js'])
     .pipe(istanbul({includeUntested: true}))
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('mocha', ['coverage'], function() {
+gulp.task('mocha', gulp.series('coverage'), function() {
   return gulp.src('test/{integration/,}*.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports())
@@ -22,17 +29,10 @@ gulp.task('mocha', ['coverage'], function() {
     }));
 });
 
-gulp.task('eslint', function() {
-  return gulp.src(['*.js', 'lib/**/*.js', 'test/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
-
 gulp.task('unused', function() {
   var utils = require('./lib/utils');
   return gulp.src(['index.js', 'lib/**/*.js'])
     .pipe(unused({keys: Object.keys(utils)}));
 });
 
-gulp.task('default', ['mocha']);
+gulp.task('default', gulp.series('mocha'));
