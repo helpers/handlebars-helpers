79f70c3c1e316a4a79a16803bb3a982387eb2d7a
dchenk <dchenk@users.noreply.github.com>
2022-01-12 21:41:48 -0800

    Add type declaration

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+import { HelperDeclareSpec } from 'handlebars';
+
+export const array: HelperDeclareSpec;
+export const code: HelperDeclareSpec;
+export const collection: HelperDeclareSpec;
+export const comparison: HelperDeclareSpec;
+export const date: HelperDeclareSpec;
+export const html: HelperDeclareSpec;
+export const i18n: HelperDeclareSpec;
+export const inflection: HelperDeclareSpec;
+export const markdown: HelperDeclareSpec;
+export const match: HelperDeclareSpec;
+export const math: HelperDeclareSpec;
+export const misc: HelperDeclareSpec;
+export const number: HelperDeclareSpec;
+export const object: HelperDeclareSpec;
+export const path: HelperDeclareSpec;
+export const regex: HelperDeclareSpec;
+export const string: HelperDeclareSpec;
+export const url: HelperDeclareSpec;
