e5174af9f7dd3f344e2a9c71d42f9c9b7ac3f053
mike12345567 <me@michaeldrury.co.uk>
2023-12-13 17:54:56 +0000

    Fixing test case.

--- test/uuid.js
+++ test/uuid.js
@@ -6,8 +6,8 @@ const uuid = require('../lib/uuid');
 
 describe('uuid', function() {
   describe('generate', function() {
-    it('should return an empty string if undefined', function() {
-      assert.match(uuid.UUID(), /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+    it('should generate a valid uuid', function() {
+      assert.match(uuid.uuid(), /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
     });
   });
 });
